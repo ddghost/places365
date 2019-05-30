ce3a6ff21c0cd35074a2fbff262f160470251782
ddghost <544011312@qq.com>
2019-05-30 09:39:15 +0800

    update

--- SENet.py
+++ SENet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 def conv3x3(in_channels, out_channels, stride=1):
     return nn.Conv2d(in_channels, out_channels, kernel_size=3, 
