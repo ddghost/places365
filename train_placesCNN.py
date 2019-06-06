f9fc48deae36241ac28be0aa61917861c00b4991
ddghost <544011312@qq.com>
2019-06-06 17:29:27 +0800

    update

--- train_placesCNN.py
+++ train_placesCNN.py
@@ -291,7 +291,7 @@ def accuracy(output, target, topk=(1,)):
     res = []
     for k in topk:
         correct_k = correct[:k].view(-1).float().sum(0) 
-        print(correct_k.shape)
+        print(correct.shape)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
 
