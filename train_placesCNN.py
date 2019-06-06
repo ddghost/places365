79e9b5b70efbc51694bfe48dc696cd7c13a59ebb
ddghost <544011312@qq.com>
2019-06-06 17:45:16 +0800

    update

--- train_placesCNN.py
+++ train_placesCNN.py
@@ -283,16 +283,13 @@ def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
     maxk = max(topk)
     batch_size = target.size(0)
-
+    print(output.shape,target.shape)
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
-    
     res = []
     for k in topk:
         correct_k = correct[:k].view(-1).float().sum(0) 
-        #correct_k = (correct[:k].sum(0) > 0.5).view(-1).float().sum(0)
-        print(correct_k,k)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
 
