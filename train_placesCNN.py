a87285fa78a338f1e00c12c2fa4ffc752507f769
ddghost <544011312@qq.com>
2019-06-07 08:51:53 +0800

    update

--- train_placesCNN.py
+++ train_placesCNN.py
@@ -302,6 +302,7 @@ def getErrorImgMask(output, target, topk=(1,)):
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
+    pred.t()
     masks = []
     for k in topk:
         mask = (correct[:k].sum(0) == 1)
