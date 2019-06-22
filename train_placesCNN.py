83f92239afc2ab7a01c58b759157edb213f31f27
ddghost <544011312@qq.com>
2019-06-22 20:03:49 +0800

    update

--- train_placesCNN.py
+++ train_placesCNN.py
@@ -153,7 +153,7 @@ def main():
         
         trainFc(trainMidOutputs, 0.01, num_epochs, criterion, optimizer, fcModel)
 
-        valMidOutputs = getMidOutputs(val_loader, fcModel)
+        valMidOutputs = getMidOutputs(val_loader, model)
         validate(valMidOutputs, fcModel, criterion)
         return
     else:
