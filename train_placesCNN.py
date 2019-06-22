f088a7d2e8dbbeb084e9856221744f56949a1ba6
ddghost <544011312@qq.com>
2019-06-22 20:23:43 +0800

    update

--- train_placesCNN.py
+++ train_placesCNN.py
@@ -154,7 +154,7 @@ def main():
         trainFc(trainMidOutputs, 0.01, num_epochs, criterion, optimizer, fcModel)
 
         valMidOutputs = getMidOutputs(val_loader, model)
-        validate(valMidOutputs, fcModel, criterion)
+        validateFc(valMidOutputs, fcModel, criterion)
         return
     else:
         optimizer = torch.optim.SGD(model.parameters(), args.lr,
@@ -542,50 +542,36 @@ def trainFc(midOutputs, learningRate, num_epochs, criterion, optimizer, fcModel)
             curr_lr /= 10
             update_lr(optimizer, curr_lr)
 
-'''
+
 def validateFc(valMidOutputs, fcModel, criterion):
-    batch_time = AverageMeter()
-    losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
+    curr_lr = learningRate
+    
+    epoch_loss = 0.0
+    total = 0
+    correct = 0
+    with torch.no_grad():
+        for i, (images, target) in enumerate(midOutputs):
 
-    # switch to evaluate mode
-    fcModel.eval()
+            fcModel.eval()
 
-    end = time.time()
-    for i, (input, target) in enumerate(valMidOutputs):
-        target = target.cuda(non_blocking=True)
-        input_var = torch.autograd.Variable(input, volatile=True)
-        target_var = torch.autograd.Variable(target, volatile=True)
+            target = target.cuda(non_blocking=True)
+            input_var = torch.autograd.Variable(images)
+            target_var = torch.autograd.Variable(target)
 
-        # compute output
-        output = fcModel(input_var)
-        loss = criterion(output, target_var)
+            outputs = fcModel(input_var)
+            loss = criterion(outputs, target_var)
+            epoch_loss += loss.item()
 
-        # measure accuracy and record loss
-        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
-        losses.update(loss.data[0], input.size(0))
-        top1.update(prec1[0], input.size(0))
-        top5.update(prec5[0], input.size(0))
+            _, predicted = torch.max(outputs.data, 1)
+            correct += (predicted == target_var).sum().item()
+            total += target_var.size(0)
+            epoch_loss /= len(midOutputs)
+            val_acc = correct / total * 100
+        
+        print ("epoch [{}/{}], avg_loss: {:.4f}, val_acc: {:.4f}"
+               .format(epoch+1, num_epochs, epoch_loss, val_acc))
 
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
 
-        if i % args.print_freq == 0:
-            print('Test: [{0}/{1}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                   i, len(val_loader), batch_time=batch_time, loss=losses,
-                   top1=top1, top5=top5))
-
-    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
-          .format(top1=top1, top5=top5))
-
-    return top1.avg
-'''
 if __name__ == '__main__':
     main()
 
