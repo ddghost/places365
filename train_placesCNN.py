adadf8f7e4f7ef1573fdfd8fb035dd4ff3615c63
ddghost <544011312@qq.com>
2019-06-23 09:31:20 +0800

    update

--- train_placesCNN.py
+++ train_placesCNN.py
@@ -493,8 +493,7 @@ def getMidOutputs(loader, model):
             batch_time.update(time.time() - end)
             end = time.time()
             bar.output(i+1)
-            if(i == 50):
-                break
+
         print()
     return midOutputs
 
