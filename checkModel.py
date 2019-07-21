b83c9ada0dbe2137beeb561a1eadabe6e5d11110
ddghost <544011312@qq.com>
2019-07-21 11:34:21 +0800

    update

--- checkModel.py
+++ checkModel.py
@@ -50,7 +50,7 @@ def main():
 	if args.resume:
 		if os.path.isfile(args.resume):
 			print("=> loading checkpoint '{}'".format(args.resume))
-			checkpoint = torch.load(args.resume).to(initCuda)
+			checkpoint = torch.load(args.resume, map_location=lambda storage, loc: storage)
 			args.start_epoch = checkpoint['epoch']
 			best_prec1 = checkpoint['best_prec1']
 			model.load_state_dict(checkpoint['state_dict'])
