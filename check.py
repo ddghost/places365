c0cd328b6e21741ce291f574dd8f7fde39dd1873
ddghost <544011312@qq.com>
2019-08-24 21:07:24 +0800

    update

--- check.py
+++ check.py
@@ -28,13 +28,53 @@ import pandas as pdb
 import csv
 
 from torchstat import stat
+parser = argparse.ArgumentParser(description='PyTorch Places365 Training')
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N', help='mini-batch size (default: 256)')
 
 def main():
+    #统计神经网络
+    args = parser.parse_args()
+    '''
     model = SENet.se_resnet50(num_classes=365)
     stat(model, (3, 224, 224))
     model = SENet.se_resnet152(num_classes=365)
     stat(model, (3, 224, 224))
+    '''
 
+    traindir = os.path.join(args.data, 'train')
+    valdir = os.path.join(args.data, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(traindir, transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+    
+    for _ in range(5):
+        print(time.asctime( time.localtime(time.time()) ))
+        for i, (input, target) in enumerate(train_loader):
+            input = input.cuda()
+            target = train.cuda()
+    print(time.asctime( time.localtime(time.time()) ))
 if __name__ == '__main__':
     main()
 
