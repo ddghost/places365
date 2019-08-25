1b947f4b2c8f29d36611d12d41bb988fb4819646
ddghost <544011312@qq.com>
2019-08-25 09:04:15 +0800

    update

--- SENet.py
+++ SENet.py
@@ -1,13 +1,16 @@
 import torch.nn as nn
 import math
 
-from torchvision.models import ResNet
-
-
 def conv3x3(in_planes, out_planes, stride=1):
-    "3x3 convolution with padding"
+    """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
 
 class SELayer(nn.Module):
     def __init__(self, channel, reduction=16):
