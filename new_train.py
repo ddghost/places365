89b2f5026b6b54d79bf181781b64b732f066bc0d
ddghost <544011312@qq.com>
2019-08-26 15:10:32 +0800

    update

--- new_train.py
+++ new_train.py
@@ -65,8 +65,8 @@ parser.add_argument('--useNewTrainMethod', dest='useNewTrainMethod', action='sto
 parser.add_argument('--num_classes',default=365, type=int, help='num of class in the model')
 parser.add_argument('--dataset',default='places365',help='which dataset to train')
 device_ids = [0]
-ini_device = 0
-best_prec1 = 0
+ini_device = 1
+best_prec1 = 1
 
 
 
